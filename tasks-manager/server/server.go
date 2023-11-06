977988b788741de3ac84ea53cf2bd51977cce262
sadath-12 <sadathsadu2002@gmail.com>
2023-11-06 12:04:20 +0530

    enhancement:  ratelimiting
    Signed-off-by: sadath-12 <sadathsadu2002@gmail.com>

--- tasks-manager/server/server.go
+++ tasks-manager/server/server.go
@@ -3,22 +3,68 @@ package server
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
+	"math/rand"
 	"sadath/tasks"
 	"sync"
+	"time"
 
 	"github.com/hibiken/asynq"
+	"golang.org/x/time/rate"
 )
+
+// Rate is 10 events/sec and permits burst of at most 30 events.
+var limiter = rate.NewLimiter(10, 30)
+
+func middleFunc(next asynq.Handler) asynq.Handler {
+	return asynq.HandlerFunc(func(ctx context.Context, t *asynq.Task) error {
+		fmt.Println("this is middleware")
+		if !limiter.Allow() {
+			return &RateLimitError{
+				RetryIn: time.Duration(rand.Intn(10)) * time.Second,
+			}
+		}
+		return nil
+	})
+}
+
+type RateLimitError struct {
+	RetryIn time.Duration
+}
+
+func (e *RateLimitError) Error() string {
+	return fmt.Sprintf("rate limited (retry in  %v)", e.RetryIn)
+}
+
+func IsRateLimitError(err error) bool {
+	_, ok := err.(*RateLimitError)
+	return ok
+}
+
+func retryDelay(n int, err error, task *asynq.Task) time.Duration {
+	var ratelimitErr *RateLimitError
+	if errors.As(err, &ratelimitErr) {
+		return ratelimitErr.RetryIn
+	}
+	return asynq.DefaultRetryDelayFunc(n, err, task)
+}
 
 // workers.go
 func Workers(wg *sync.WaitGroup) {
 	defer wg.Done()
 	srv := asynq.NewServer(
 		asynq.RedisClientOpt{Addr: "localhost:6379"},
-		asynq.Config{Concurrency: 10},
+		asynq.Config{Concurrency: 10,
+			// If error is due to rate limit, don't count the error as a failure.
+			IsFailure:      func(err error) bool { return !IsRateLimitError(err) },
+			RetryDelayFunc: retryDelay,
+		},
 	)
 
 	mux := asynq.NewServeMux()
+	mux.Use(middleFunc)
 	mux.HandleFunc("email:welcome", sendWelcomeEmail)
 	mux.HandleFunc("email:reminder", sendReminderEmail)
 	mux.HandleFunc("example_task", sendExample)
